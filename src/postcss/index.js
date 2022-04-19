d248690dfca6a99d8a1eaaddd56897919df31bda
陶雅阁 <taoyage@taoyagedeMacBook-Pro.local>
2022-04-19 11:50:45 +0800

    feat: add postcss-flexbugs-fixes and postcss-normalize plugin

--- src/postcss/index.js
+++ src/postcss/index.js
@@ -1,13 +1,20 @@
 module.exports = {
   plugins: [
     require.resolve('postcss-image-set-polyfill'),
+    require.resolve('postcss-flexbugs-fixes'),
     [
       require.resolve('postcss-preset-env'),
       {
-        autoprefixer: {},
+        autoprefixer: {
+          flexbox: 'no-2009',
+        },
         browsers: 'last 2 versions',
         stage: 3,
       },
     ],
+    // Adds PostCSS Normalize as the reset css with default options,
+    // so that it honors browserslist config in package.json
+    // which in turn let's users customize the target behavior as per their needs.
+    require.resolve('postcss-normalize'),
   ],
 };
